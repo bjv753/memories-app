a723d484f7e579ffe258eff71c920124eeeee1e0
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-01 15:45:43 -0500

    update

--- client/src/Components/Posts/Posts.js
+++ client/src/Components/Posts/Posts.js
@@ -12,9 +12,13 @@ const Posts = () => {
 
   return (
     !posts.length ? <CircularProgress /> : (
-      <Grid className={classes.container}>
-
-      </Grid>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {posts.map((post) => (
+              <Grid key={post._id} item xs={12} sm={6} >
+                  <Post post={post} />
+              </Grid>
+          ))}
+          </Grid>
     )
   )
 }
