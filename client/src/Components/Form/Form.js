0f514e00df74f96ae755129f600b737f04c97fe0
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-01 12:28:52 -0500

    update

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { TextField, Button, Typography, Paper } from '@material-ui/core'
+
 import useStyles from './styles'
 
-
 const Form = () => {
-      const classes = useStyles()
+  const [postData, setPostData] = useState({
+    creator: '', title: '', message: '', tags: '', selectedFile: ''
+  })
+  const classes = useStyles()
+
+  const handleSubmit = () => {}
+
   return (
-    <div>Form</div>
+    <Paper className={classes.paper}>
+      <form
+        autoComplete='off'
+        noValidate
+        className={classes.form}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant='h6'>Creating a Memory</Typography>
+        <TextField
+          name='creator'
+          variant='outlined'
+          label='Creator'
+          fullWidth
+          value={postData.creator}
+          onChange={}
+        ></TextField>
+      </form>
+    </Paper>
   )
 }
 
