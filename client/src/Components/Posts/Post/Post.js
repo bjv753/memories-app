e8f171100bcb31ea00e8d39e1c5e72bf71f33dd5
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-04 12:27:07 -0500

    update

--- client/src/Components/Posts/Post/Post.js
+++ client/src/Components/Posts/Post/Post.js
@@ -33,17 +33,17 @@ const Post = ( {post, setCurrentId} ) => {
         </div>
         <Typography className={classes.title} variant="h5" gutterBottom>{post.title}</Typography>
         <CardContent>
-        <Typography  variant="body2" color="textSecondary" gutterBottom>{post.message}</Typography>
+        <Typography  variant="body2" color="textSecondary" component="p" >{post.message}</Typography>
         </CardContent>
         <CardActions className={classes.cardActions}>
             <Button size="small" color="primary" onClick={() => dispatch(likePost(post._id))} >
                 <ThumbUpAltIcon  fontsize="small" />
-                Like 
+                &nbsp; Like &nbsp;
                 {post.likeCount}
             </Button>
             <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} >
                 <DeleteIcon  fontsize="small" />
-                Delete 
+               &nbsp; Delete &nbsp;
             </Button>
         </CardActions>
     </Card>
