8d87a21de79e071124a04343d7634f1f32cb6a1c
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-03 20:37:39 -0500

    update

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -50,6 +50,6 @@ export const likePost = async (req, res) => {
   const post = await PostMessage.findById(id)
   const updatePost = await PostMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true })
 
-  res.json(updatedPost)
+  res.json(updatePost)
 
 }
