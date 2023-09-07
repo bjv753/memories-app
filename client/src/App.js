94d67b2b91f0f961b60e0bc105cc60b1e3993935
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-07 15:59:13 -0500

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,27 @@
-import React, { useState, useEffect } from 'react'
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import { useDispatch } from 'react-redux'
+import React, { useState, useEffect } from 'react';
+import { Container, Grow, Grid } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
 
-import { getPosts } from './actions/posts'
-import Posts from './components/Posts/Posts'
-import Form from './components/Form/Form'
-import memories from './images/memories.png'
-import useStyles from './styles'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import Navbar from './components/Navbar/Navbar';
+import { getPosts } from './actions/posts';
+import useStyles from './styles';
+
 
 const App = () => {
   const [currentId, setCurrentId] = useState(null)
-  const classes = useStyles()
-  const dispatch = useDispatch()
+  const classes = useStyles();
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPosts())
-  }, [currentId, dispatch])
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
 
   return (
     <div>
-      <Container maxwidth='lg'>
-        <AppBar className={classes.appBar} position='static' color='inherit'>
-          <Typography variant='h2' align='center'>
-            Memories
-          </Typography>
-          <img
-            className={classes.image}
-            src={memories}
-            alt='memories'
-            height='60'
-          />
-        </AppBar>
+      <Container maxWidth='lg'>
+        <Navbar/>
         <Grow in>
           <Container>
             <Grid
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react'; 
+import {Link} from 'react-router-dom';
+import { AppBar, Typography } from '@material-ui/core';
+import useStyles from './styles';
+import memories from '../../images/memories.png'
 
 const Navbar = () => {
+      const classes = useStyles();
+
   return (
       <AppBar className={classes.appBar} position='static' color='inherit'>
-      <Typography variant='h2' align='center'>
-        Memories
-      </Typography>
-      <img
-        className={classes.image}
-        src={memories}
-        alt='memories'
-        height='60'
-      />
+            <div className={classes.brandContainer} >
+                  <Typography  component={Link} to="/" variant='h2' align='center'>Memories</Typography>
+                  <img className={classes.image} src={memories} alt='memories' height='60'/>
+            </div>
     </AppBar>
   )
 }
 
-export default Navbar
+export default Navbar;
