a0f09b0b226c0d07b0a661f34fe41c77336dba27
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-09 10:50:10 -0500

    update

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Avatar, Button, Paper, Grid, TextField, Typography, Container } from '@material-ui/core';
+import React, {useState} from 'react';
+import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
+import Input from './Input';
 
 const Auth = () => {
       const classes = useStyles();
+      const [showPassword, setShowPassword] = useState(false)
+
       const isSignup = false;
+
+      const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
       const handleSubmit = () => {
 
@@ -27,12 +32,10 @@ const Auth = () => {
                               {
                                     isSignup && (
                                           <>
-                                          <Grid xs={6} md={12}>
-                                                <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6} />
-                                          </Grid>
-                                          <Grid xs={6} md={12}>
-                                                <TextField name="firstName" label="First Name" handleChange={handleChange} autoFocus xs={6} />
-                                          </Grid>
+                                                <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                                                <Input name="firstName" label="First Name" handleChange={handleChange}  half />
+                                                <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+                                                <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
                                           </>
                                     )
                               }
