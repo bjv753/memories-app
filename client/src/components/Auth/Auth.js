0ab10fbc5d35f99235961fc2b2d238e617d7b1ab
Benjamin Vredevelt <bjv753@gmail.com>
2023-09-09 11:34:42 -0500

    update

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -8,7 +8,7 @@ const Auth = () => {
       const classes = useStyles();
       const [showPassword, setShowPassword] = useState(false)
 
-      const isSignup = false;
+      const isSignup = true;
 
       const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
 
@@ -29,17 +29,19 @@ const Auth = () => {
                   <Typography variant="h5">{isSignup ? 'Sign Up' : 'Sign In'}</Typography>
                   <form className={classes.form} onSubmit={handleSubmit}>
                         <Grid container spacing={2}>
-                              {
-                                    isSignup && (
+                              { isSignup && (
                                           <>
                                                 <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
                                                 <Input name="firstName" label="First Name" handleChange={handleChange}  half />
-                                                <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
-                                                <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
                                           </>
-                                    )
-                              }
+                                          )}
+                                                <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} />
+                                                <Input name="email" label="Email Address" handleChange={handleChange} type="email"/>
+                                                { isSignup && <Input name="confirmPassword" label="Repeat Password" handleChange={handleChange} type="password" /> }
                         </Grid>
+                        <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit} >
+                              {isSignup ? 'Sign Up' : 'Sign In'}
+                        </Button>
                   </form>
             </Paper>
     </Container>
